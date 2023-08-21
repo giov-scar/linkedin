f259e86c910647106b0fc951381aa6bef1aa64e1
andreas ubertini <ubertiniandreas@gmail.com>
2023-08-21 12:15:22 +0200

    working

--- src/app/models/auth-interceptor.ts
+++ src/app/models/auth-interceptor.ts
@@ -15,7 +15,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     let newRequest = request.clone({
-      headers: request.headers.append('Authentication', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTQ5ODFmMTc1YzAwMTRjNTU4YzUiLCJpYXQiOjE2OTI2MDM1NDQsImV4cCI6MTY5MzgxMzE0NH0.C7V4YeMJUDOKFb22KMObyy1n4VdVkNsHVKGLxOnJqNE')
+      headers: request.headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTQ5ODFmMTc1YzAwMTRjNTU4YzUiLCJpYXQiOjE2OTI2MDM1NDQsImV4cCI6MTY5MzgxMzE0NH0.C7V4YeMJUDOKFb22KMObyy1n4VdVkNsHVKGLxOnJqNE')
     })
 
     return next.handle(newRequest);

f598e083e4e5e56f9c28768a1763f096fafe16ec
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2023-08-21 16:56:24 +0200

    fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   API_KEY:
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTQ5ODFmMTc1YzAwMTRjNTU4YzUiLCJpYXQiOjE2OTI2MDM1NDQsImV4cCI6MTY5MzgxMzE0NH0.C7V4YeMJUDOKFb22KMObyy1n4VdVkNsHVKGLxOnJqNE\n ',
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTI5MzFmMTc1YzAwMTRjNTU4YmYiLCJpYXQiOjE2OTI2MDMwMjcsImV4cCI6MTY5MzgxMjYyN30.i-GoNFsrlKGgNdqcvfELZyDPcKM-c-FVc0z9jypW9N4  ',
   BASE_URL: 'https://striveschool-api.herokuapp.com/api/profile/',
 };

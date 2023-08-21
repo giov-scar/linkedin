1806922c15da0bb3711992bca5258dd0dac18d8c
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2023-08-21 11:08:39 +0200

    installo npm i + environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  API_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTI5MzFmMTc1YzAwMTRjNTU4YmYiLCJpYXQiOjE2OTI2MDMwMjcsImV4cCI6MTY5MzgxMjYyN30.i-GoNFsrlKGgNdqcvfELZyDPcKM-c-FVc0z9jypW9N4',
+};

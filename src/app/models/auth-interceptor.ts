cfdd18bc7a115b888c1f0f5196282e46f70d8b16
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2023-08-21 12:39:47 +0200

    modified

--- src/app/models/auth-interceptor.ts
+++ src/app/models/auth-interceptor.ts
@@ -3,20 +3,24 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     let newRequest = request.clone({
-      headers: request.headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTQ5ODFmMTc1YzAwMTRjNTU4YzUiLCJpYXQiOjE2OTI2MDM1NDQsImV4cCI6MTY5MzgxMzE0NH0.C7V4YeMJUDOKFb22KMObyy1n4VdVkNsHVKGLxOnJqNE')
-    })
+      headers: request.headers.set(
+        'Authorization',
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTI5MzFmMTc1YzAwMTRjNTU4YmYiLCJpYXQiOjE2OTI2MDMwMjcsImV4cCI6MTY5MzgxMjYyN30.i-GoNFsrlKGgNdqcvfELZyDPcKM-c-FVc0z9jypW9N4 '
+      ),
+    });
 
     return next.handle(newRequest);
   }

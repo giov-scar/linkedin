83aa37e1558358eae264e168156f7fbbfb3ef7dc
simone-congiu <simone44c@gmail.com>
2023-08-21 12:57:54 +0200

    fist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,15 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NavComponent } from './components/nav/nav.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthInterceptor} from "./models/auth-interceptor";
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './models/auth-interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    NavComponent
+  declarations: [AppComponent, ProfileComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
+  providers: [
+    [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    HttpClientModule
-  ],
-  providers: [  [ { provide: HTTP_INTERCEPTORS, useClass:
-    AuthInterceptor, multi: true } ]],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

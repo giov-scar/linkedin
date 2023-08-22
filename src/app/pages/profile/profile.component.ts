5911cf1c2049038bd26ace34b9e544a0abf18405
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2023-08-22 11:55:09 +0200

    fix

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,52 +1,53 @@
-import {Component, OnInit} from '@angular/core';
-import {ProfileService} from "../../profile.service";
-import {Observable} from "rxjs";
-import {IApiResp} from "../../models/iapi-resp";
-import {ExperienceService} from "../../experience.service";
-import {ExpApiResp} from "../../models/exp-api-resp";
-
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../profile.service';
+import { Observable } from 'rxjs';
+import { IApiResp } from '../../models/iapi-resp';
+import { ExperienceService } from '../../experience.service';
+import { ExpApiResp } from '../../models/exp-api-resp';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit{
-  data!: IApiResp
-  expData!:ExpApiResp
+export class ProfileComponent implements OnInit {
+  data!: IApiResp;
+  expData!: ExpApiResp;
 
-  constructor(private profileSvc: ProfileService, private expService: ExperienceService) {
-
-  }
+  constructor(
+    private profileSvc: ProfileService,
+    private expService: ExperienceService
+  ) {}
   ngOnInit() {
-    // this.getMyProfile()
+    this.getMyProfile();
     // this.getSpecific()
-     // this.getAll()
-    this.getMyexp()
+    // this.getAll()
+    this.getMyexp();
   }
 
   getAll() {
-    this.profileSvc.getAllProfile().subscribe(data => {
-      console.log(data)
-      this.data = data
-    })
+    this.profileSvc.getAllProfile().subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    });
   }
 
-   getSpecific() {
-     this.profileSvc.getSpecificProfile('60fab669fab8670015d6bc99').subscribe(data => {
-       console.log(data)
-       this.data = data
-     })
-   }
+  getSpecific() {
+    this.profileSvc
+      .getSpecificProfile('60fab669fab8670015d6bc99')
+      .subscribe((data) => {
+        console.log(data);
+        this.data = data;
+      });
+  }
 
-   getMyProfile(){
-    this.profileSvc.getMyProfile().subscribe(data => {
-      console.log(data)
-      this.data = data
-    })
-   }
-   getMyexp(){
-    this.expService.getAllExp().subscribe(expData=>console.log(expData))
-   }
-
+  getMyProfile() {
+    this.profileSvc.getMyProfile().subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    });
+  }
+  getMyexp() {
+    this.expService.getAllExp().subscribe((expData) => console.log(expData));
+  }
 }

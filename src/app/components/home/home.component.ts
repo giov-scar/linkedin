51ae57241fc28e92feb941bea4aa2cab89359151
giov-scar <giovanniscarantino@gmail.com>
2023-08-22 18:36:28 +0200

    style on home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IApiResp } from 'src/app/models/iapi-resp';
+import { ProfileService } from 'src/app/profile.service';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data!: IApiResp
+  constructor(private profileSvc: ProfileService,){}
+  ngOnInit(): void {
+    this.getMyProfile()
+  }
 
+
+getMyProfile() {
+  this.profileSvc.getMyProfile().subscribe(data => {
+    console.log(data)
+    this.data = data
+
+  })
+}
 }

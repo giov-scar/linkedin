30cae4be0d5459e8ba149d43114cb5354e29184d
andreas ubertini <ubertiniandreas@gmail.com>
2023-08-24 14:29:23 +0200

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,12 @@
-import { FormBuilder, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { IApiResp } from 'src/app/models/iapi-resp';
-import { PostApiResp } from 'src/app/models/post-api-resp';
-import { PostService } from 'src/app/post.service';
-import { ProfileService } from 'src/app/profile.service';
+import {FormBuilder, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {IApiResp} from 'src/app/models/iapi-resp';
+import {PostApiResp} from 'src/app/models/post-api-resp';
+import {PostService} from 'src/app/post.service';
+import {ProfileService} from 'src/app/profile.service';
 import {Icommentapi} from "../../models/icommentapi";
-// import {FullPost} from "../../models/full-post";
+import {FullPost} from "../../models/full-post";
 
 @Component({
   selector: 'app-home',
@@ -15,19 +15,22 @@ import {Icommentapi} from "../../models/icommentapi";
 })
 export class HomeComponent implements OnInit {
   data!: IApiResp;
-  allComments!:Icommentapi[];
+  allComments!: Icommentapi[];
   allPost!: PostApiResp[];
-  // AllFullPost!:FullPost[];
+  AllFullPost!: FullPost[];
   toDelete: string = '64e713d4ad2497001469364b';
   formPost!: FormGroup;
-  maxPostToDisplay!:PostApiResp[]
-  maxCommentToDisplay!:Icommentapi[]
+  maxPostToDisplay!: PostApiResp[]
+  maxCommentToDisplay!: Icommentapi[]
   toSeeComments: string = 'tt0399295'
+
   constructor(
     private profileSvc: ProfileService,
     private postSvc: PostService,
     private fb: FormBuilder
-  ) {}
+  ) {
+  }
+
   ngOnInit(): void {
     this.getMyProfile();
     this.getAll();
@@ -49,7 +52,7 @@ export class HomeComponent implements OnInit {
       console.log(data);
       data.reverse()
       this.allPost = data;
-      this.maxPostToDisplay = data.slice(0,50)
+      this.maxPostToDisplay = data.slice(0, 50)
       this.getAllComments()
 
     })
@@ -58,20 +61,23 @@ export class HomeComponent implements OnInit {
   createPost() {
     this.postSvc
       .insertNewPost(this.formPost.value)
-      .subscribe((data) => this.getAll());
+      .subscribe((data) => {
+        this.getAll()
+        this.formPost.reset()
+      });
   }
 
-  deletePost() {
-    console.log(this.toDelete);
-    this.postSvc.deletePost(this.toDelete).subscribe((data) => this.getAll());
+  deletePost(toDelete:string) {
+    this.postSvc.deletePost(toDelete).subscribe((data) => this.getAll());
   }
 
-  getAllComments(){
-    this.postSvc.getAllComments().subscribe((data:Icommentapi[]): void =>{
+  getAllComments() {
+    this.postSvc.getAllComments().subscribe((data: Icommentapi[]): void => {
       console.log(data, 'commenti')
       data.reverse()
       this.allComments = data
-      this.maxCommentToDisplay = data.slice(0,50).reverse()
+      this.maxCommentToDisplay = data.slice(0, 50).reverse()
     })
   }
+
 }

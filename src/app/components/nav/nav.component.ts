250a4b2a549f9383d7343ed5a84155f6ca584292
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2023-08-24 17:27:32 +0200

    fix nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,38 +1,36 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {ProfileService} from "../../profile.service";
-import {IApiResp} from "../../models/iapi-resp";
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ProfileService } from '../../profile.service';
+import { IApiResp } from '../../models/iapi-resp';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements AfterViewInit {
-  data!: IApiResp
-  isActive: Boolean = false
-  class:string = 'col-lg-3'
+  data!: IApiResp;
+  isActive: Boolean = false;
+  class: string = 'col-lg-3';
+
   constructor(private profileSvc: ProfileService) {
-    this.getMyProfile()
-
+    this.getMyProfile();
   }
-  @ViewChild('search') search!: ElementRef
+  @ViewChild('search') search!: ElementRef;
   ngAfterViewInit() {
-    console.log(this.search.nativeElement.status)
-
+    console.log(this.search.nativeElement.status);
   }
-  getMyProfile(){
-    this.profileSvc.getMyProfile().subscribe(data => {
-      this.data = data
-    })
+  getMyProfile() {
+    this.profileSvc.getMyProfile().subscribe((data) => {
+      this.data = data;
+    });
   }
 
- changeStat(){
-   this.isActive = !this.isActive
-   if(this.isActive){
-     this.class = 'col-lg-5'
-   }else{
-     this.class = 'col-lg-3'
-   }
- }
-
+  changeStat() {
+    this.isActive = !this.isActive;
+    if (this.isActive) {
+      this.class = 'col-lg-5';
+    } else {
+      this.class = 'col-lg-3';
+    }
+  }
 }

cb5ca1ce81ee3197b675dd5bb4e44c55e96dee4b
giov-scar <giovanniscarantino@gmail.com>
2023-08-25 09:21:22 +0200

    style comment section

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   API_KEY:
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTI2YzFmMTc1YzAwMTRjNTU4YmUiLCJpYXQiOjE2OTI2MDI5ODgsImV4cCI6MTY5MzgxMjU4OH0.tw_FBF69PPmRf_AuQIMSizjvTGanmHhw4XcBEGXJ8J8 ',
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTRlNzFmMTc1YzAwMTRjNTU4YzciLCJpYXQiOjE2OTI2MDM2MjMsImV4cCI6MTY5MzgxMzIyM30.C2UYyPcCoiSvT6m-yWr4foxvga780RWvxSj3qUG5WhE ',
   BASE_URL: 'https://striveschool-api.herokuapp.com/api/profile/',
   POST_URL: 'https://striveschool-api.herokuapp.com/api/posts/',
   COMMENT_URL: 'https://striveschool-api.herokuapp.com/api/comments/',
